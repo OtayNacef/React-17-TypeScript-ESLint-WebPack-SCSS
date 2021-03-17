0c41a112c0430140480258452cb611c9bfd7b94e
Nacef Otay <nacef.otay@esprit.tn>
2021-03-18 00:55:31 +0100

    Create React 17  TypeScript With ESLint , WebPack & SCSS

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.scss";
+const App: React.FC = () => (
+  <div className="wrapper">
+    <h1>React 17 and TypeScript 4 App!🚀</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,60 @@
+import path from "path";
+import { Configuration, DefinePlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
+const webpackConfig = (): Configuration => ({
+  entry: "./src/index.tsx",
+  ...(process.env.production || !process.env.development
+    ? {}
+    : { devtool: "eval-source-map" }),
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
+  },
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "build.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /build/,
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles
+      template: "./public/index.html",
+    }),
+    // DefinePlugin allows you to create global constants which can be configured at compile time
+    new DefinePlugin({
+      "process.env": process.env.production || !process.env.development,
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      // Speeds up TypeScript type checking and ESLint linting (by moving each to a separate process)
+      eslint: {
+        files: "./src/**/*.{ts,tsx,js,jsx}",
+      },
+    }),
+  ],
+});
+
+export default webpackConfig;
