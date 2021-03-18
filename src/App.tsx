fa9cfca2941e0e77a6fed570690b046260bdca80
Nacef Otay <nacef.otay@esprit.tn>
2021-03-18 10:48:20 +0100

    Adding Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import "./styles.scss";
+import React from 'react';
+import './styles.scss';
+
 const App: React.FC = () => (
-  <div className="wrapper">
-    <h1>React 17 and TypeScript 4 App!🚀</h1>
-  </div>
+    <div className="wrapper">
+        <h1>React 17 and TypeScript 4 App!🚀</h1>
+    </div>
 );
 
 export default App;
